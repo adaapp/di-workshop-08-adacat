2cfef9ad0d0793f9f342f031f1d3ea7439c22c8b
✨ Alex ✨ <alex@dytry.ch>
2018-10-24 18:10:55 +0100

    add health

--- AdaCat.js
+++ AdaCat.js
@@ -18,9 +18,19 @@ class AdaCat {
   }
 
   getDescription() {
+    var sleepLine
+    if (this.isSleeping) {
+      sleepLine = 'Shh! ' + this.name + ' is sleeping.'
+    } else {
+      sleepLine = this.name + ' is awake.'
+    }
     var lines = [
       this.name + ' is a cat. they belong to ' + this.owner + '.',
-      'their hunger level is ' + this.hunger + '/10.'
+      'their hunger level is ' + this.hunger + '/10.',
+
+      'they weigh ' + this.size + ' tonnes.',
+      'their health is ' + this.getHealth() + '/30.',
+      sleepLine
     ]
 
     return lines.join('\n')
@@ -46,7 +56,34 @@ class AdaCat {
 
   play() {
     var hunger = this.hunger + 3
+    if (hunger > 7) {
+      this.size = this.size - 1
+    }
     this.setHunger(hunger)
+  }
+
+  getHealth() {
+    // the ideal weight for cats is 30
+    // this futher they are from this, the less
+    // healthy they are
+    var sizeDifferenceFromIdeal = Math.abs(this.size - 30)
+
+    // sizeScore starts at thirty, and gets
+    // smaller as the cat's size gets further
+    // from the ideal weight
+    var sizeScore = 30 - sizeDifferenceFromIdeal
+
+    // health score gets lower as the cat gets
+    // more hungry
+    var healthScore = sizeScore - this.hunger
+
+    // max returns the biggest value, so health
+    // will never go below 0
+    if (healthScore < 0) {
+      healthScore = 0
+    }
+
+    return healthScore
   }
 }
 
--- AdaCat.test.js
+++ AdaCat.test.js
@@ -50,6 +50,35 @@ describe('AdaCat', function() {
       var lines = result.split('\n')
       expect(lines[1]).to.equal('their hunger level is 5/10.')
     })
+
+    it('tells you if the cat is awake', function() {
+      var myCat = new AdaCat('marmite', 'alex')
+      var result = myCat.getDescription()
+      var lines = result.split('\n')
+      expect(lines[4]).to.equal('marmite is awake.')
+    })
+
+    it('tells you if the cat is asleep', function() {
+      var myCat = new AdaCat('mc splinters', 'alex')
+      myCat.nap()
+      var result = myCat.getDescription()
+      var lines = result.split('\n')
+      expect(lines[4]).to.equal('Shh! mc splinters is sleeping.')
+    })
+
+    it('includes the cat size', function() {
+      var myCat = new AdaCat('oak', 'alex')
+      var result = myCat.getDescription()
+      var lines = result.split('\n')
+      expect(lines[2]).to.equal('they weigh 30 tonnes.')
+    })
+
+    it('includes the health level', function() {
+      var myCat = new AdaCat('professor dangle', 'alex')
+      var result = myCat.getDescription()
+      var lines = result.split('\n')
+      expect(lines[3]).to.equal('their health is 25/30.')
+    })
   })
 
   describe('#feed', function() {
@@ -103,6 +132,111 @@ describe('AdaCat', function() {
       myCat.hunger = 9
       myCat.play()
       expect(myCat.hunger).to.equal(10)
+    })
+
+    it('decreases size when hunger is above 7', function() {
+      var myCat = new AdaCat('ada', 'alex')
+      myCat.hunger = 8
+      myCat.play()
+      expect(myCat.size).to.equal(29)
+    })
+  })
+
+  describe('#getHealth', function() {
+    it('is 30 when size = 30, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 30
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(30)
+    })
+
+    it('is 25 when size = 25, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 25
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(25)
+    })
+
+    it('is 25 when size = 35, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 35
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(25)
+    })
+
+    it('is 5 when size = 5, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 5
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(5)
+    })
+
+    it('is 5 when size = 55, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 55
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(5)
+    })
+
+    it('is 0 when size = 65, hunger = 0', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 65
+      myCat.hunger = 0
+      var result = myCat.getHealth()
+      expect(result).to.equal(0)
+    })
+
+    it('is 23 when size = 30, hunger = 7', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 30
+      myCat.hunger = 7
+      var result = myCat.getHealth()
+      expect(result).to.equal(23)
+    })
+
+    it('is 18 when size = 25, hunger = 7', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 25
+      myCat.hunger = 7
+      var result = myCat.getHealth()
+      expect(result).to.equal(18)
+    })
+
+    it('is 18 when size = 35, hunger = 7', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 35
+      myCat.hunger = 7
+      var result = myCat.getHealth()
+      expect(result).to.equal(18)
+    })
+
+    it('is 2 when size = 5, hunger = 3', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 5
+      myCat.hunger = 3
+      var result = myCat.getHealth()
+      expect(result).to.equal(2)
+    })
+
+    it('is 2 when size = 55, hunger = 3', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 55
+      myCat.hunger = 3
+      var result = myCat.getHealth()
+      expect(result).to.equal(2)
+    })
+
+    it('is 0 when size = 1, hunger = 3', function() {
+      var myCat = new AdaCat('bort', 'alex')
+      myCat.size = 1
+      myCat.hunger = 3
+      var result = myCat.getHealth()
+      expect(result).to.equal(0)
     })
   })
 })
