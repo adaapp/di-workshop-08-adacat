a3403f88b02d8040fdd44b8e2780dd9e6228bdb0
✨ Alex ✨ <alex@dytry.ch>
2018-10-24 17:19:31 +0100

    add hunger and feeding

--- AdaCat.js
+++ AdaCat.js
@@ -2,10 +2,19 @@ class AdaCat {
   constructor(name, owner) {
     this.name = name
     this.owner = owner
+    this.hunger = 5
   }
 
   getDescription() {
     return this.name + ' is a cat. they belong to ' + this.owner + '.'
+  }
+
+  feed() {
+    var hunger = this.hunger - 1
+    if (hunger < 0) {
+      hunger = 0
+    }
+    this.hunger = hunger
   }
 }
 
--- AdaCat.test.js
+++ AdaCat.test.js
@@ -5,19 +5,24 @@ var AdaCat = require('./AdaCat')
 
 describe('AdaCat', function() {
   it('is a class', function() {
-    var myCat = new AdaCat()
+    var myCat = new AdaCat('jeff', 'alex')
     expect(myCat).to.be.an.instanceOf(AdaCat)
   })
 
   describe('#constructor', function() {
     it('takes name and sets it as an attribute', function() {
-      var myCat = new AdaCat('taffy')
+      var myCat = new AdaCat('taffy', 'alex')
       expect(myCat.name).to.equal('taffy')
     })
 
     it('takes owner and sets it as an attribute', function() {
       var myCat = new AdaCat('buttons', 'alex')
       expect(myCat.owner).to.equal('alex')
+    })
+
+    it('sets the hunger attribute to 5', function() {
+      var myCat = new AdaCat('greg', 'alex')
+      expect(myCat.hunger).to.equal(5)
     })
   })
 
@@ -26,6 +31,21 @@ describe('AdaCat', function() {
       var myCat = new AdaCat('decking', 'alex')
       var result = myCat.getDescription()
       expect(result).to.equal('decking is a cat. they belong to alex.')
+    })
+  })
+
+  describe('#feed', function() {
+    it('decreases the hunger attribute by 1', function() {
+      var myCat = new AdaCat('mittens', 'alex')
+      myCat.feed()
+      expect(myCat.hunger).to.equal(4)
+    })
+
+    it('does not decrease hunger below 0', function() {
+      var myCat = new AdaCat('leggy', 'alex')
+      myCat.hunger = 0
+      myCat.feed()
+      expect(myCat.hunger).to.equal(0)
     })
   })
 })
